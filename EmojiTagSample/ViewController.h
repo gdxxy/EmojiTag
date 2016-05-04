d0585b9e474e817fbaf5b809eee099c2f727135a
xiexianyu <xianyuxie@gmail.com>
2016-05-04 11:00:03 +0800

    Initial Commit

---
+++ EmojiTagSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmojiTagSample
+//
+//  Created by xiexianyu on 5/4/16.
+//  Copyright © 2016 QIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
